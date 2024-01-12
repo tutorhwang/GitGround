bfe55f1c72ee5440efdd770739f836d520631ddf
tutorhwang <heeya.hwang@gmail.com>
2024-01-12 12:37:43 +0900

    [implement] Add rotation animation button and implement its functionality

--- app/src/main/java/com/example/gitground/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gitground/ui/home/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.example.gitground.ui.home
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -33,6 +36,25 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            animationButton.setOnClickListener {
+                val rotate = ObjectAnimator.ofFloat(textView, "rotation", 0f, 1080f).apply {
+                    interpolator = AccelerateInterpolator()
+                }// 360 * 3
+                val moveRight =
+                    ObjectAnimator.ofFloat(textView, "translationX", 0f, 500f)
+
+                AnimatorSet().apply {
+                    playTogether(rotate, moveRight)
+                    duration = 1000L
+                    start()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
